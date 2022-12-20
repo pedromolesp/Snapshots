3d0dad6d69b20cd08bfaf16a10842ddade9eba55
Pedro Molina <pedro.molina@petroprix.com>
2022-12-20 18:42:53 +0100

    terminada

--- app/src/main/java/com/example/snapshots/Snapshot.kt
+++ app/src/main/java/com/example/snapshots/Snapshot.kt
@@ -7,6 +7,4 @@ data class Snapshot(var id:String = "",
                     var title: String="",
                     var photoUrl:String="",
                     var likeList:Map<String, Boolean> = mutableMapOf()
-                    ){
-
-}
+                    )
